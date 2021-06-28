fec0ed96e4976fba783944a268fe0c73ce9af493
Kaustubh Manglurkar <kaustubh1517@gmail.com>
2021-06-28 21:40:42 +0530

    practice-done

---
+++ app.js
@@ -0,0 +1,31 @@
+var txtInput = document.querySelector("#input");
+var btnTranslate = document.querySelector("#btn");
+var outputDiv = document.querySelector("#output");
+
+var serverURL = "";
+
+
+btnTranslate.addEventListener("click", clickHandler);
+
+function clickHandler() {
+
+    // var inputText = txtInput.value;
+
+    // console.log("clicked!")
+
+    fetch(serverURL)
+     .then(response => response.json())
+     .then(json => {
+
+        outputDiv.innerText = "Kaustubh";
+     })
+
+     .catch(errorHandler)
+}
+
+function errorHandler(error) {
+
+    // console.log("error occured", error)
+    alert("Something wrong with server! Try again after some time");
+
+}
